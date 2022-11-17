eb1d841b42e17d8136ea901557ff9c4123ec6658
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-17 07:58:28 -0500

    Started lab, vague idea laid out for plan of attack

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,18 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    Object[][] oa;
+
+    public SinglyLinkedList(int size) {
+        oa = new Object[size][2];
+    }
+
+    public void add(Object o) {
+        Object[][] temp = new Object[oa.length][2];
+        for (int i=0; i<oa.length; i++) {
+            temp[i][1] = oa[i][1];
+        }
+        temp[oa.length][1] = o;
+        oa = temp;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,24 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.testng.annotations.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList sll;
+
+//    add -- add an element to the list
+    @Test
+    public void testAdd() {
+        sll = new SinglyLinkedList(2);
+        sll.add(5);
+    }
+//    remove -- remove an element (specified by numeric index) from the list
+//    contains -- returns true if the element is in the list, false otherwise
+//    find -- returns the element's index if it is in the list, -1 otherwise
+//    size -- returns the current size of the list
+//    get -- returns the element at the specified index
+//    copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+//    sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
 }
