b1dd78116db287a3836c70004ad79ff5c6f862a4
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-29 13:28:10 -0500

    Add,Remove,Contains,Find,Size,Get, and Copy completed

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,21 +1,97 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
     Object[][] oa;
 
-    public SinglyLinkedList(int size) {
-        oa = new Object[size][2];
+    public SinglyLinkedList() {
+        oa = new Object[1][2];
     }
 
+    public SinglyLinkedList(Object[][] oda) {
+        oa = oda;
+    }
+
+
     public void add(Object o) {
-        Object[][] temp = new Object[oa.length][2];
+        Object[][] temp = new Object[oa.length+1][2];
         for (int i=0; i<oa.length; i++) {
+            temp[i][0] = i;
             temp[i][1] = oa[i][1];
         }
+        temp[oa.length][0] = oa.length;
         temp[oa.length][1] = o;
+//        remove(null);
         oa = temp;
+//        for (int i=0; i<size(); i++) {
+//            System.out.println(oa[i][1]);
+//        }
+    }
+
+    public void remove(Object o) {
+        Object[][] temp = new Object[oa.length-1][2];
+        int j = 0;
+        for (int i=0; i<oa.length; i++) {
+            Object t = oa[i][1];
+            try {
+                if (!t.equals(o)) {
+                    temp[j][0] = j;
+                    temp[j][1] = oa[i][1];
+                    j++;
+                }
+            } catch (NullPointerException e) {
+                continue;
+            }
+        }
+        oa = temp;
+//        for (int i=0; i<size(); i++) {
+//            System.out.println(oa[i][1]);
+//        }
+//        for (int i=0; i<oa.length; i++) {
+//            if (oa[i][1].equals(null)) remove(null);
+//        }
+    }
+
+    public int size() {
+        return oa.length;
+    }
+
+    public boolean contains(Object o) {
+        for (int i=0; i<size(); i++) {
+            if (oa[i][1].equals(o)) return true;
+        }
+        return false;
+    }
+
+    public int find(Object o) {
+        for (int i=0; i<size(); i++) {
+            if (oa[i][1].equals(o)) return i;
+        }
+        return -1;
+    }
+
+    public Object get(int o) {
+        return oa[o][1];
+    }
+
+    public SinglyLinkedList copy() {
+        Object[][] temp = new Object[oa.length][2];
+        for (int i=0; i<oa.length; i++) {
+            temp[i][0] = i;
+            temp[i][1] = oa[i][1];
+        }
+        return new SinglyLinkedList(temp);
+    }
+
+    public void sort() {
+        int count = 0;
+        Object[][] sorted = new Object[oa.length][2];
+        while (count<oa.length) {
+            
+        }
     }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -8,17 +9,83 @@ import org.testng.annotations.Test;
 public class SinglyLinkedListTest {
     SinglyLinkedList sll;
 
+
 //    add -- add an element to the list
     @Test
     public void testAdd() {
-        sll = new SinglyLinkedList(2);
+        sll = new SinglyLinkedList();
         sll.add(5);
+        sll.add(6);
+
+        sll.remove(5);
     }
 //    remove -- remove an element (specified by numeric index) from the list
+    @Test
+    public void testRemove() {
+        sll = new SinglyLinkedList();
+        sll.add(5);
+        sll.add(6);
+        sll.remove(6);
+        sll.remove(null);
+        Assert.assertEquals(1, sll.size());
+    }
 //    contains -- returns true if the element is in the list, false otherwise
+    @Test
+    public void testContains() {
+        sll = new SinglyLinkedList();
+        sll.add(3);
+        sll.remove(null);
+
+        Assert.assertTrue(sll.contains(3));
+    }
 //    find -- returns the element's index if it is in the list, -1 otherwise
+    @Test
+    public void testFind() {
+        sll = new SinglyLinkedList();
+        sll.add("Ritter");
+        sll.add("Sheba");
+        sll.remove(null);
+
+        Assert.assertEquals(0, sll.find("Ritter"));
+    }
 //    size -- returns the current size of the list
+    @Test
+    public void testSize() {
+        sll = new SinglyLinkedList();
+        sll.add(1);
+        sll.add(2);
+        sll.remove(null);
+        Assert.assertEquals(2, sll.size());
+    }
 //    get -- returns the element at the specified index
+    @Test
+    public void testGet() {
+        sll = new SinglyLinkedList();
+        sll.add("Ritter");
+        sll.add(42);
+        sll.remove(null);
+        Assert.assertEquals(42, sll.get(1));
+    }
 //    copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+    @Test
+    public void testCopy() {
+        sll = new SinglyLinkedList();
+        sll.add("Ritter");
+        sll.add(42);
+        sll.remove(null);
+        SinglyLinkedList sll2 = sll.copy();
+        Assert.assertEquals(42, sll2.get(1));
+    }
 //    sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
+    @Test
+    public void testSort() {
+        sll = new SinglyLinkedList();
+        sll.add(42);
+        sll.add("Marvin");
+        sll.add("Deep Thought");
+        sll.remove(null);
+        sll.sort();
+        Assert.assertEquals("Marvin", sll.get(2));
+    }
+
 }
